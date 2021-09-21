e8200a535328fb3907705ee3ebdf2d8197825598
Tysyatsky <Tysyatsky@users.noreply.github.com>
2021-09-21 21:27:00 +0300

    Update Player.cpp

--- Player.cpp
+++ Player.cpp
@@ -4,7 +4,28 @@ Player::Player() {
   points = 0;
   cash = 15000;
   bankrot = false;
-  pos.x = 0;
-  pos.y = 0;
+  pos = 0;
   fieldCount = 0;
+}
+Player::Player(int*& money) {
+  points =0;
+  cash = money;
+  bankrot = false;
+  pos = 0;
+  fieldCount = 0;
+}
+Player::~Player(){
+  points = -1;
+  cash = 0;
+  bankrot = true;
+  pos = -1;
+  fieldCount = -1;
+}
+Player::ifBankrot(int money, bool*& bankrot) {
+  if (money <= 0) {
+    bankrot = true;
+  }
+}
+Player::buy(int*& money){
+
 }
