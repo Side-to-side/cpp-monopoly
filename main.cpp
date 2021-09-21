c3555b1bf5029d0c70ee7f5b885b5496880546de
Kenobichek <76412011+Kenobichek@users.noreply.github.com>
2021-09-21 22:42:41 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,16 +1,5 @@
-﻿#include <iostream>
+#include <iostream>
 
 int main(){
     std::cout << "Hello Monopoly!\n";
-    std::cout << "/* message */" << '\n';
-    s
-        t
-        a
-        r
-        o
-        d
-        u
-        b
-        b
-        b
 }
