ba7710ff5c4646e15b489e15dec00e53442bb17d
Kenobichek <lesechklesia@gmail.com>
2021-09-24 22:51:12 +0300

    upgraded

--- Monopoly.cpp
+++ Monopoly.cpp
@@ -1,13 +1,25 @@
 #include "Monopoly.h"
 
 void Monopoly::menu() {
-
+	std::cout << "MENU:\n";
+	std::cout << "number of players: ";
+	int n;
+	std::cin >> n;
+	numberPlayers = n;
+	std::cout << "number of bots: ";
+	std::cin >> n;
+	numberBots = n;
 }
 
 void Monopoly::startGame() {
-
+	int id = 0;
+	for (int i = 0; i < numberBots; i++) {
+		AI a("Bot", id);
+		vecAI.push_back(a);
+		id++;
+	}
 }
 
 void Monopoly::updateGame() {
-
+	
 }
--- Monopoly.h
+++ Monopoly.h
@@ -1,11 +1,15 @@
 #pragma once
+#include <iostream>
+#include <vector>
+#include "AIplayer.h"
+
 class Monopoly {
 public:
 	void menu();		// функция меню
 	void startGame();	// функция с начальными данными 
 	void updateGame();	// сама игра 
 private:
-	int NumberPlayers;
-	int NumberBots;
-
+	int numberPlayers;
+	int numberBots;
+	std::vector<AI> vecAI;
 };
