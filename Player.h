166fcebecb01eba8378160c8eb3dd6391c9c173f
Tysyatsky <Tysyatsky@users.noreply.github.com>
2021-09-26 21:22:38 +0300

    Update Player.h

--- Player.h
+++ Player.h
@@ -14,22 +14,4 @@ public:
 private:
 };
 
-/*class Player {
-private:
-	int points;
-	int cash;
-	bool bankrot;
-	int pos;
-	int fieldCount;
-public:
-	Player();
-	Player(int Tpoints, int Tcash, bool Tbankrot, int Tpos, int Tfield);
-	~Player();
-	bool buy(int money);
-	bool ifBankrot(int money);
-	int getPos();
-	int getPoints();
-	int getCash();
-	int getField();
-	//int pointCount(int points);
-};*/
+
