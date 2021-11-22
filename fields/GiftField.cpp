028951cf404e8911b8c3dc01c884ff815bb3eed4
Olesya Lesechko <76412011+Kenobichek@users.noreply.github.com>
2021-11-22 20:49:29 +0200

    Update GiftField.cpp

--- fields/GiftField.cpp
+++ fields/GiftField.cpp
@@ -3,13 +3,9 @@
 void GiftField::action(std::unique_ptr<AbstractPlayer>& player) {
 
 	std::random_device generation;
-
 	int value = (generation() % 50 + 1) * 100;
-
-  int cash = player->getCash();
-
-  cash += value;
-
+	int cash = player->getCash();
+	cash += value;
 	player->setCash(cash);
 
 }
